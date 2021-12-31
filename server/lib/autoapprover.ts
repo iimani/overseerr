4dab2c83c60e920846aea6441cc79c367435fca5
iimani <ismailimani92@gmail.com>
2021-12-31 17:23:51 +0100

    feat(remove async call, update gitignore): remove async call, update gitignore

--- server/lib/autoapprover.ts
+++ server/lib/autoapprover.ts
@@ -4,34 +4,37 @@ import { MediaRequestStatus } from '../constants/media';
 import logger from '../logger';
 
 class AutoApprover {
-  public async autoApprove(): Promise<void> {
+  public autoApprove(): void {
     const requestRepository = getRepository(MediaRequest);
     const now = new Date();
     logger.debug(`Now: ${now}`);
     const threshold = new Date(new Date(now).setMinutes(now.getMinutes() - 5));
     logger.debug(`Threshold: ${threshold}`);
     try {
-      const requests = await requestRepository.find({
+      const requests = requestRepository.find({
         where: {
           status: MediaRequestStatus.PENDING,
         },
       });
-      logger.debug(`Found ${requests.length} pending requests`);
-
-      for (const request of requests) {
-        if (now >= threshold) {
-          request.status = MediaRequestStatus.APPROVED;
-          logger.debug(
-            `Approved request for ${request.media.id}, createdAt: ${request.createdAt}`
-          );
-        } else {
-          logger.debug(
-            `Skipped request ${request.media.id}, createdAt: ${request.createdAt}`
-          );
-        }
-      }
-
-      await requestRepository.save(requests);
+      const updateRequests: MediaRequest[] = [];
+      requests.then((response) => {
+        response.forEach((request) => {
+          if (request.createdAt >= threshold) {
+            request.status = MediaRequestStatus.APPROVED;
+            updateRequests.push(request);
+            logger.debug(
+              `Approved request for ${request.media.id}, createdAt: ${request.createdAt}`
+            );
+          } else {
+            logger.debug(
+              `Skipped request ${request.media.id}, createdAt: ${request.createdAt}`
+            );
+          }
+        });
+        logger.debug(`Updating ${updateRequests.length} requests`);
+        requestRepository.save(updateRequests);
+        // return response.status(200).json();
+      });
     } catch (e) {
       logger.error('Error processing request update', {
         label: 'Media Request',
